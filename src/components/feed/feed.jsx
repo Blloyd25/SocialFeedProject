db06883cbd9dd1946cd182334e671fd9eadd96c4
benjamin lloyd <benjamin.a.lloyd25@gmail.com>
2022-03-08 15:24:25 -0500

    working on feed css and js

---
+++ src/components/feed/feed.jsx
@@ -0,0 +1,13 @@
+import Share from "../share"
+import "./feed.css"
+
+export default function Feed() {
+    return(
+        <div className="feed">
+            <div className="feedWrapper">
+                <Share/>
+                </div>
+            
+        </div>
+    )    
+}
---
+++ src/components/home.jsx
@@ -0,0 +1,12 @@
+import Feed from "./feed/feed"
+import "./home.css"
+
+export default function Home() {
+    return(
+        <>
+        <div className="homecontainer">
+            <Feed/>
+        </div>
+        </>
+    )
+}
