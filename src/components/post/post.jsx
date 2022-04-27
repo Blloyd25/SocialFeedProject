051a2895bb06f65dd9fe17d08280e14ac42cd09e
Blloyd25 <benjamin.a.lloyd25@gmail.com>
2022-04-27 12:00:08 -0400

    created post jsx and css files

---
+++ src/components/post/post.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+const Post = (props) => {
+
+
+        const [name, setName] = useState([])
+        const [body, setBody] = useState([])
+        // const [entries, setEntries] = useState([])
+
+        // useEffect(()=>{
+        //     console.log("from post", entries)
+        // })
+        
+
+        function handleSubmit(submissionForm){
+            submissionForm.preventDefault();
+            let newEntry = {
+                name: name,
+                body: body
+            };
+            console.log(newEntry)
+            props.addNewEntryProperty(newEntry)
+        }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>Name:</label>
+                <input type='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+                <label>Whats this?</label>
+                <input type='body' value={body} onChange={(e) => setBody(e.target.value)}/>
+                <button type='submit'>Submit</button>
+                
+            </form>
+        </div>
+    );
+}
+
+export default Post;
